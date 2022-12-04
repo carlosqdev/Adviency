1ce0258e7097f2e61c84343637e2d273dc33e6a5
carlosqdev <carlosquintero95nic@gmail.com>
2022-12-03 23:07:13 -0600

    Advency Dia 3

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,50 @@ import React from 'react'
 import './styles.css'
 
 function App() {
+  const [list, setList] = React.useState([
+    {
+      id: 1,
+      element: 'Computadora'
+    },
+    {
+      id: 2,
+      element: 'Teclado'
+    },
+    {
+      id: 3,
+      element: 'Mouse'
+    }
+  ]);
+  const [item, setItem] = React.useState({
+    id: new Date().getTime() + 10,
+    element: ''
+  });
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    setList([...list, item])
+  }
+
+  function handleChange(e: any) {
+    const { name, value } = e.currentTarget
+    setItem({ ...item, [name]: value })
+  }
+
   return (
-    <div>
-      <h1>hello world!</h1>
+    <div className='app'>
+      <section className='gift'>
+        <h1 className='gift-title'>Lista de regalos</h1>
+        <form className='gift-form' onSubmit={handleSubmit}>
+          <label className='gift-label' htmlFor="element">Agregar regalo:</label>
+          <input className='gift-input' value={item.element} name='element' id='element' onChange={handleChange} type="text" placeholder='Escribe tu regalo' />
+          <button className='gift-button' type="submit">Agregar</button>
+        </form>
+        <ul className='gift-list'>
+          {list?.map((item) => (
+            <li key={item.id} className='gift-list__item'>{item.element}</li>
+          ))}
+        </ul>
+      </section>
     </div>
   )
 }
